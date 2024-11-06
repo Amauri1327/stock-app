6c43ff898e1cb63d470121d20e377038362bc928
Amauri1327 <amauri1816@hotmail.com>
2024-11-05 21:31:14 -0300

    instalacao e configuracao Nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -3,16 +3,8 @@ import { StyleSheet, View, Text } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
+    <View className='flex-1 justify-center items-center bg-yellow-50'>
+      <Text className='text-3xl font-semibold text-gray-700'>Hello World</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    }
-});
