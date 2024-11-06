6b81b2211b9b4e848eff2857d994bcda574ebf11
Amauri1327 <amauri1816@hotmail.com>
2024-11-06 20:45:23 -0300

    implementacao input pesquisar produto

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
-import { StyleSheet, View, Text, Button, Touchable, TouchableOpacity, } from 'react-native';
-import Ionicons from '@expo/vector-icons/Ionicons';
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, TextInput } from "react-native";
+import { Feather, Ionicons } from "@expo/vector-icons";
 
 export default function HomeScreen() {
+  const [text, setText] = useState('');
+
   return (
-    <View className='flex-1 justify-center items-center bg-yellow-50'>
-      <Text className='text-3xl font-semibold text-gray-700'>Hello World</Text>
-      <TouchableOpacity 
-          className='absolute top-28 left-6  bg-blue-500 rounded-3xl'
-          onPress={() => console.log('Press')}
+    <View className="flex-1 justify-between items-center">
+      <Text className="text-3xl font-semibold text-gray-700"></Text>
+      <TouchableOpacity
+        className="absolute top-28 left-6  bg-blue-500 rounded-3xl"
+        onPress={() => console.log("Press")}
       >
-          <Ionicons name="add" size={70} color="white" /> 
+        <Ionicons name="add" size={70} color="white" />
       </TouchableOpacity>
+      <View className="flex-1 m-24">
+        <TextInput
+          className="bg-gray-200 text-black left-4 text-2xl w-64 p-2 rounded-lg border"
+          placeholder="Pesquisar produto"
+          onChangeText={setText}
+          value={text}
+          onSubmitEditing={()=> {
+            console.log("Teclado");
+            setText('');
+          }}
+        ></TextInput>
+        <Feather className="left-56 -top-10" name="search" size={28} color="black" />
+      </View>
     </View>
   );
 }
