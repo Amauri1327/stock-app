39b8af11cace6d896da099104bb199f740144f46
Amauri1327 <amauri1816@hotmail.com>
2024-11-07 23:17:20 -0300

    criacao database initialize, e modal inicial

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,13 @@
+import { SQLiteProvider } from "expo-sqlite"
 import "../global.css"
 
 import { Slot } from "expo-router"
+import { initializeDatabase } from "@/database/initializeDatabase"
 
 export default function Layout(){
     return (
-        <Slot />
+        <SQLiteProvider databaseName="myDatabase.db" onInit={initializeDatabase}>
+            <Slot />
+        </SQLiteProvider>
     )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,64 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity, TextInput } from "react-native";
+import { View, Text, TouchableOpacity, TextInput, Modal } from "react-native";
 import { Feather, Ionicons } from "@expo/vector-icons";
-
 export default function HomeScreen() {
+
   const [text, setText] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
 
   return (
+
+
+
+
+
     <View className="flex-1 justify-between items-center">
-      <Text className="text-3xl font-semibold text-gray-700"></Text>
-      <TouchableOpacity
-        className="absolute top-28 left-6  bg-blue-500 rounded-3xl"
-        onPress={() => console.log("Press")}
-      >
-        <Ionicons name="add" size={70} color="white" />
-      </TouchableOpacity>
+      <View className="-left-60">
+        <Text className="text-3xl font-semibold text-gray-700"></Text>
+
+
+        <TouchableOpacity
+          className="absolute top-28 left-6  bg-blue-500 rounded-3xl"
+          onPress={() => setModalVisible(true)}
+        >
+          <Ionicons name="add" size={70} color="white" />
+        </TouchableOpacity>
+
+        <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => setModalVisible(false)}
+        >  
+        <View className="flex-1 justify-center items-center bg-gray-300">
+          <View className="w-72 p-20 bg-white rounded-3xl">
+          <Text className="text-2xl font-bold mb-4">Modal Simples</Text>
+            <Text>Essa é uma modal simples com uma mensagem.</Text>
+
+
+            <TouchableOpacity
+                className="mt-5 bg-slate-200 p-3 rounded-md items-center border"
+                onPress={() => setModalVisible(false)} 
+            >
+              <Text className="text-black text-2xl">Fechar</Text>
+
+            </TouchableOpacity>
+          </View>
+        </View>
+        </Modal>
+      </View>
+      
+
+
+
+
+
+
+
+
+
+
+
       <View className="flex-1 m-24">
         <TextInput
           className="bg-gray-200 text-black left-4 text-2xl w-64 p-2 rounded-lg border"
